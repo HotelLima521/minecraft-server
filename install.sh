cebcaf6436024e2d342ec4dddd2ad61563d68135
derek <derek@hl521.me>
2022-05-11 19:55:16 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
-tmp='/tmp/'
+tmp='/tmp'
 archive='hl521-minecraft-2022-05-11.tar.gz'
 checksum='hl521-minecraft-2022-05-11.tar.gz.sha512sum'
 minecraft='~/.minecraft'
+BYel='\e[1;33m';
+Whi='\e[0;37m';
+Cya='\e[0;36m';
 wget -c https://archives.hl521.me/tarball/$archive https://archives.hl521.me/checksums/$checksum
 sha512sum -c $tmp/$checksum
 notify-send "Placeholder for sha512sum verification"
 mkdir minecraft
 tar -xzf $tmp/$archive -C $tmp/minecraft
-echo -n "Would you like to install the Forge Client? (Only do this if it needs to be updated, or you don't currently have it)\nY/n"
+printf "Would you like to install the Forge Client? ${BYel}(Only do this if it needs to be updated, or you don't currently have it)\n${Cya}## ${Whi}Y/n ${Cya}## ${Whi}-> "
 read INPUT
 case $INPUT in
 	Y | y)
