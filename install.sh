a127ad7de6512036439e7e05a7cda77343c02e62
derek <derek@hl521.me>
2022-05-12 15:06:37 -0600

    Update shell script

--- install.sh
+++ install.sh
@@ -1,25 +1,29 @@
 #!/bin/sh
-
+########################################################
 tmp='/tmp'
-archive='hl521-minecraft-2022-05-11.tar.gz'
-checksum='hl521-minecraft-2022-05-11.tar.gz.sha512sum'
+archive='hl521-minecraft-2022-05-10.tar.gz'
+checksum='hl521-minecraft-2022-05-10.tar.gz.sha512sum'
 minecraft='~/.minecraft'
-Yel='\e[0,33m';
-BYel='\e[1;33m';
-Whi='\e[0;37m';
-Cya='\e[0;36m';
-printf "${Yel}Checking for dependencies..."
+yellow='\e[0,33m';
+boldyellow='\e[1;33m';
+green='\e[0;92m';
+boldgreen='\e[1;92m';
+white='\e[0;37m';
+cyan='\e[0;36m';
+reset='\[0m';
+########################################################
+printf "${yellow}Checking for dependencies...\n"
 sleep 1
-printf "${Cya}Checking for Java..."
+printf "${cyan}Checking for Java...\n"
 sleep 1
 if [java] then
-	printf "${Cya}Checking for Minecraft..."
+	printf "${cyan}Checking for Minecraft...\n"
 	sleep 1
 	if [ -d ~/.minecraft ] then
-		printf "${Cya}Checking for Forge..."
+		printf "${cyan}Checking for Forge...\n"
 		sleep 1
 		if [ -d ~/.minecraft/mods ] && [ -d ~/.minecraft/shaderpacks ] && [ -d ~/.minecraft/resourcepacks ] then
-			printf "Dependencies Check Good!\nInstalling the goods... :)"
+			printf "${green}Dependencies Check Good!\n{${boldgreen}Installing the goods... :)${reset}\n"
 			wget -c https://archives.hl521.me/tarball/$archive https://archives.hl521.me/checksums/$checksum
 			sha512sum -c $tmp/$checksum
 			mkdir minecraft
@@ -31,34 +35,34 @@ if [java] then
 			rm $tmp/$archive
 			rm $tmp/$checksum
 		else
-			printf "${BYel}Looks like Forge may not be installed..."
+			printf "${boldyellow}Looks like Forge may not be installed...\n"
 			sleep 1
-			printf "${BYel}Would you like to install it?"
-			printf "${Cya}## ${Whi}Y/n ${Cya}## ${Whi}-> "
+			printf "${boldyellow}Would you like to install it?\n"
+			printf "${cyan}## ${white}Y/n ${cyan}## ${white}-> "
 			read INPUT
 			case $INPUT in
 				Y | y)
 					java -jar $tmp/forge-1.18.2-40.1.0-installer.jar
-					printf "Re-run script now that forge is installed :)"
+					printf "Re-run script now that forge is installed :)${reset}\n"
 					sleep 5
 				;;
 				N | n)
-					printf "Okay, quitting."
+					printf "Okay, quitting.${reset}\n"
 					sleep 2
 				;;
 				*)
-					printf "Not Understanding Input"
+					printf "Not Understanding Input${reset}\n"
 					sleep 2
 				;;
 			esac
 		
 		fi
 	else
-		printf "Minecraft is not installed. Please install it to continue, then rerun the script. https://minecraft.net/"
+		printf "Minecraft is not installed. Please install it to continue, then rerun the script. https://minecraft.net/${reset}\n"
 		sleep 2
 	fi
 else
-	printf "Java is not installed. Please rectify this issue. Suggest OpenJDK, install with your package manager."
+	printf "Java is not installed. Please rectify this issue. Suggest OpenJDK, install with your package manager.${reset}\n"
 	sleep 2
 fi
 
--- update.sh
+++ update.sh
@@ -1,5 +1,9 @@
-zip -r hl521-minecraft-$(date '+%Y-%m-%d'.zip * -x 'install.*' -x '*.tar.gz'
-tar --exclude='install.*' --exclude='*.zip' -czvf hl521-minecraft-$(date '+%Y-%m-%d').tar.gz *
-sha512sum hl521-minecraft-$(date '+%Y-%m-%d').zip > hl521-minecraft-$(date '+%Y-%m-%d').zip.sha512sum
-sha512sum hl521-minecraft-$(date '+%Y-%m-%d').tar.gz > hl521-minecraft-$(date '+%Y-%m-%d').tar.gz.sha512sum
+#zip -r hl521-minecraft-$(date '+%Y-%m-%d'.zip * -x 'install.*' -x '*.tar.gz'
+zip -r hl521-minecraft-2022-05-10.zip * -x 'install.*' -x '*.tar.gz' -x 'mods_test'
+#tar --exclude='install.*' --exclude='*.zip' -czvf hl521-minecraft-$(date '+%Y-%m-%d').tar.gz *
+tar --exclude='install.*' --exclude='*.zip' --exclude='mods_test' -czvf hl521-minecraft-2022-05-10.tar.gz *
+sha512sum hl521-minecraft-2022-05-10.zip > hl521-minecraft-2022-05-10.zip.sha512sum
+#sha512sum hl521-minecraft-$(date '+%Y-%m-%d').zip > hl521-minecraft-$(date '+%Y-%m-%d').zip.sha512sum
+sha512sum hl521-minecraft-2022-05-10.tar.gz > hl521-minecraft-2022-05-10.tar.gz.sha512sum
+#sha512sum hl521-minecraft-$(date '+%Y-%m-%d').tar.gz > hl521-minecraft-$(date '+%Y-%m-%d').tar.gz.sha512sum
 # Secure Copy Below
